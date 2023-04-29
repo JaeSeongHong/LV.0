7f594f18c9f3d186f51bf3cf2ee5bd05b2abe58e
JaeSeongHong <hjs1144@gmail.com>
2023-04-29 19:56:55 +0900

    docs : 메서드

---
+++ Ex06메서드/src/메서드/Ex02매o리o.java
@@ -0,0 +1,19 @@
+package 메서드;
+
+public class Ex02매o리o {
+
+	public static void main(String[] args) {
+		// sum(5,3)
+		// 8
+		sum(5,3);
+		System.out.println(sum(5,3));
+	}
+	
+	//main  //return 타입이 있는경우 확인해보고싶으면 syso
+	public static int sum(int num1, int num2) {
+		return num1+num2;
+	}
+	
+	
+
+}
---
+++ Ex06메서드/src/메서드/Ex04매x리o.java
@@ -0,0 +1,22 @@
+package 메서드;
+
+public class Ex04매x리o {
+
+	public static void main(String[] args) {
+		
+		getName();
+		System.out.println(getName());
+		//"홍재성"
+	}
+	
+	//getName() 만들기 : 매개변수x 리턴타입o
+	//() ---> ()
+	
+	public static String getName() {
+		//리턴타입에 자료형이 있으면 return이라는 키워드가 따라온다
+		return "홍재성";
+	}
+	
+	
+	
+}
