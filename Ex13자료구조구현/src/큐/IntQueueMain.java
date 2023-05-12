eaac0b97b6574b431923b8938a07d0485bfbf92c
JaeSeongHong <hjs1144@gmail.com>
2023-05-12 17:49:37 +0900

    docs : 자료구조 실습

---
+++ Ex13자료구조구현/src/큐/IntQueueMain.java
@@ -0,0 +1,43 @@
+package 큐;
+
+import java.util.Scanner;
+
+public class IntQueueMain {
+
+	public static void main(String[] args) {
+		//삽입 삭제 피크 클리어 인덱스오브 사이즈 엠티 풀 덤프
+		Scanner sc = new Scanner(System.in);
+		
+		IntQueue q = new IntQueue(5);
+		
+		int select;
+		while(true) {
+			System.out.println("[1]인큐 [2]디큐 [3]피크 [4]클리어 [5]인덱스오브 [6]사이즈 [7]엠티 [8]풀 [9]덤프 [10]종료");
+			select = sc.nextInt();
+			if(select==1) {
+				
+			}else if(select==2) {
+				
+			}else if(select==3) {
+				
+			}else if(select==4) {
+				
+			}else if(select==5) {
+				
+			}else if(select==6) {
+				
+			}else if(select==7) {
+				
+			}else if(select==8) {
+				
+			}else if(select==9) {
+				
+			}else {
+				break;
+			}
+
+		}
+				
+	}
+
+}
